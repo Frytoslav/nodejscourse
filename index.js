dc18eab2a82890ee7510b473982d448f6c00232e
Frytoslav <filipbujakowski1@gmail.com>
2026-02-24 23:19:51 +0100

    Second module

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Happy developing ✨')
+function buildObject(keys, values) {
+    const result = {};
+    for (let i = 0; i < keys.length; i++) {
+        result[keys[i]] = values[i];
+    }
+}
